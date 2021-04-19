40cb4efb2f98fe98deae149dfb79020ceedd2e99
shubham1708 <61815446+shubham1708@users.noreply.github.com>
2021-04-19 10:09:41 +0530

    Create Raju And His Trip.cpp
    Shubham Yadav
    Manav Rachna University
    Problem Code : VALTRI
    Contest Name : CCSTART2

---
+++ Raju And His Trip.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+#define ll long long
+int main() {
+	// your code goes here
+	ll n;
+	cin>>n;
+	if(n%5==0 || n%6==0){
+	    cout<<"YES"<<endl;
+	}else{
+	    cout<<"NO"<<endl;
+	}
+}
